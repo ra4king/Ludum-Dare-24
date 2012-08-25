77d7cf5fdea6367df1968ad6201cde6f067efcea
ra4king <devnull@localhost>
2012-08-25 16:16:43 -0400

    Removed all OpenGL classes and switching to Java2D

---
+++ src/com/ra4king/ld24/Arena.java
@@ -0,0 +1,7 @@
+package com.ra4king.ld24;
+
+import com.ra4king.gameutils.gameworld.GameWorld;
+
+public class Arena extends GameWorld {
+	
+}
---
+++ src/com/ra4king/ld24/LudumDare24.java
@@ -0,0 +1,21 @@
+package com.ra4king.ld24;
+
+import com.ra4king.gameutils.Game;
+
+public class LudumDare24 extends Game{
+	private static final long serialVersionUID = 1L;
+	
+	public static void main(String[] args) {
+		LudumDare24 game = new LudumDare24();
+		game.setupFrame("Ludum Dare 24", false);
+	}
+	
+	public LudumDare24() {
+		super(800, 600);
+	}
+	
+	@Override
+	public void initGame() {
+		setScreen("Arena", new Arena());
+	}
+}
